f6e68a2ab2e5741bbcbbb380c5ce89c169058c5e
VIKRAM R <116066854+VIKRAMvikram776@users.noreply.github.com>
2022-10-28 10:48:24 +0530

    Add files via upload

---
+++ Assignmentoperators.java
@@ -0,0 +1,27 @@
+/*Example program for Assignment-operators*/
+package sample1;
+public class Assignmentoperators {
+public static void main(String[] args) {
+int num1 = 12; 
+num1 +=5; //Action of num1=num1+5
+System.out.println("The output of num1+=5 is " + num1); 
+num1 -= 5; //Action of num1=num1-5
+System.out.println("The output of num1-=5 is " + num1);
+num1 *= 5; //Action of num1=num1*5
+System.out.println("The output of num1*=5 is " + num1); 
+num1 /= 5; //Action of num1=num1/5
+System.out.println("The output of num1/=5 is " + num1); 
+num1 %= 5; //Action of num1=num1%5
+System.out.println("The output of num1%=5 is " + num1);
+}
+}
+
+
+/*OUTPUT
+The output of num1+=5 is 17
+The output of num1-=5 is 12
+The output of num1*=5 is 60
+The output of num1/=5 is 12
+The output of num1%=5 is 2
+
+-VIKRAM R*/
